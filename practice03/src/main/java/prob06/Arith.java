c273b369bb9b564b17932db1d60f70eea1406a51
sS2S2bin <3dltnqls@naver.com>
2024-04-26 11:32:13 +0900

    ADDITIONAL : 3-6과제 추상클래스에 protected 접근지시자 추가

--- practice03/src/main/java/prob06/Arith.java
+++ practice03/src/main/java/prob06/Arith.java
@@ -1,7 +1,7 @@
 package prob06;
 
 public abstract class Arith {
-	int a,b;
+	protected int a,b; //자식>부모 클래스, 같은 패키지끼리 만 공
 	public abstract int calculate();
 	public void setValue(int a, int b) {
 		this.a = a;
