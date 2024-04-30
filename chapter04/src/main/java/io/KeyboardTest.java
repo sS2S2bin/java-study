0c7af6c633f3ae732d5f7b0c3a60c04beb086b65
sS2S2bin <3dltnqls@naver.com>
2024-04-30 15:11:28 +0900

    4.3 키보드 입력 스트림 구

---
+++ chapter04/src/main/java/io/KeyboardTest.java
@@ -0,0 +1,44 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KeyboardTest {
+
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		
+		try {
+			//1. 기반 스트림(표준입력, stdin, System.in)
+			
+			//2. 보조스트림01(byte|byte|bute -> char)
+			InputStreamReader isr = new InputStreamReader(System.in, "utf-8");
+			
+			//3. 보조스트림02(char1|char2|char3|\n -> "char1char2char3")
+			br = new BufferedReader(isr);
+			
+			//4. 처리
+			String line = null;
+			while((line = br.readLine())!=null) {
+				if("quit".equals(line)) {
+					break;
+				}
+				System.out.println(line);
+				
+			}
+		}catch(IOException e) { //unsupportedexception도 얘 자식인가봐	
+			System.out.println("error"+e);
+		}finally {
+			if(br!=null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
