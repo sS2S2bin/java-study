4d4f3b6207a5e8aa3cbd383f67896e804a75569c
sS2S2bin <3dltnqls@naver.com>
2024-05-01 14:42:51 +0900

    5.2 TCP Server #finish

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -17,14 +20,63 @@ public class TCPServer {
 			//2. 바인딩(binding)
 			// Socket에 InetSocketAddress[InetAddress(IPAddress + Port)]룰 바인딩 한다.
 			// IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다.
-			serverSocket.bind(new InetSocketAddress("0.0.0.0",5000),10);
+			serverSocket.bind(new InetSocketAddress("0.0.0.0",5001),10);
 			
 			
 			//3. accept
 			Socket socket = serverSocket.accept(); //blocking
 			
 			
-			System.out.println("연결 완료");
+			
+			// data 통신 
+			try {
+			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+			int remotePort = inetRemoteSocketAddress.getPort();
+			System.out.println("[server] connected by client[" +remoteHostAddress +":" + remotePort + "]");
+			
+			//4. IO Stream 받아오기
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+			
+			
+
+			while(true) {
+				//5. 데이터 읽기
+				// int data = is.read(); 이건 한 글자씩
+				//소켓통신은 바이트 단위1!!!!!!
+				byte[] buffer = new byte[256];
+				int readByteCount = is.read(buffer); //blocking
+				
+				if(readByteCount==-1) {
+					// client 가 정상적으로 종료
+					System.out.println("[server] closed by client");
+					break;
+				}
+				String data = new String(buffer,0,readByteCount,"UTF-8");
+				System.out.println("[server] received: "+data);
+				
+				
+				//6.데이터 쓰기
+				os.write(data.getBytes("utf-8"));
+			}
+	
+			}catch(SocketException e) {
+				System.out.println("[server] 비정상 종료 suddenly closed by client : "+e);
+			}
+			catch (IOException e) {
+				System.out.println("[server] error : "+e);
+			}finally {
+				if (socket !=null && !socket.isClosed()) {
+					try {
+						socket.close();
+						System.out.println("client socket 닫힘");
+					} catch (IOException e) {
+						System.out.println("[client] error : "+e);
+					}
+				}
+			}
+			
 			
 			
 		} catch (IOException e) {
