12fa1f24f6cb2be4d70a69b76c1f8bd447d216a0
sS2S2bin <3dltnqls@naver.com>
2024-05-01 15:11:58 +0900

    ADD: NSLookup #solve

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -9,15 +9,23 @@ public class NSLookup {
 	public static void main(String[] args) {
 		//이거 숙제
 		Scanner scanner = new Scanner(System.in);
-		System.out.print(">>");
-		String domain = scanner.nextLine();
-		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName(domain);
+		while(true) {
+			System.out.print(">> ");
+			String domain = scanner.nextLine();
+			if ("exit".equals(domain)) {
+				break;
+			}
+			try {
+				InetAddress[] inetAddresses = InetAddress.getAllByName(domain);
+				for (int i = 0; i<inetAddresses.length;i++) {
+					System.out.println(domain + " : "+ inetAddresses[i]);
+				}
+				
+			}
+			catch(UnknownHostException e){
+				System.out.println("잘못된 도메인 주소입니다. "+e);
+			}
 		}
-		catch(UnknownHostException e){
-			System.out.println("잘못된 도메인 주소입니다. "+e);
-		}
-		
 		
 	}
 
